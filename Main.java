6559da7b6d181fdb61c53e9bed5c6e41db293a7e
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-15 12:40:02 +0000

    made it tell you which of the two numbers you entered are bigger

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * this program will tell the user which of the two numbers they put in is bigger
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,21 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
-  }
-}
+    Scanner input = new Scanner(System.in);
+    System.out.println ("Please enter an integer");
+    int first_Number = input.nextInt();
+    System.out.println("Please enter a second integer");
+    int second_Number = input.nextInt();
+
+    if (first_Number > second_Number) {
+      System.out.println (+ first_Number + " is bigger than " + second_Number + ".");
+    }
+      else if (first_Number < second_Number) {
+        System.out.println (+ second_Number + " is bigger than " + first_Number + "." );
+      }
+        else if (first_Number == second_Number) {
+          System.out.println (+ first_Number + " is equal to " + second_Number + ".");
+        }
+
+      }
+    }
